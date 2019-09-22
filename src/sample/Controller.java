7fe8aac9c30f4b4f8b9e74f85d4df9bb274cad89
Fernando Ramirez <feramirez5725@eagle.fgcu.edu>
2019-09-21 22:47:29 -0400

    No errors on my code but has issues running. 1 Bug, no style problems. Can't comprehend the messages intelliJ is giving me.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,58 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+  @FXML private ComboBox<Integer> DisplayList;
+
+  /**
+   * DiplayProduction will print "Product" when clicked
+   *
+   * @param event
+   */
+  @FXML
+  void DiplayProduction(MouseEvent event) {
+    System.out.println("Product");
+  }
+
+  /**
+   * RecordProduct will print "Recorded" when clicked
+   *
+   * @param event
+   */
+  @FXML
+  void RecordProduct(MouseEvent event) {
+    System.out.println("Recorded");
+  }
+
+  /**
+   * DisplayList will display dataBase list when clicked
+   *
+   * @param event
+   */
+  @FXML
+  void DisplayList(MouseEvent event) {}
+
+  /** Initialize() initiates comboBox on scene builder */
+  @FXML
+  void initialize() {
+    DisplayList.setEditable(true);
+    DisplayList.getItems().add(1);
+    DisplayList.getItems().add(2);
+    DisplayList.getItems().add(3);
+    DisplayList.getItems().add(4);
+    DisplayList.getItems().add(5);
+    DisplayList.getItems().add(6);
+    DisplayList.getItems().add(7);
+    DisplayList.getItems().add(8);
+    DisplayList.getItems().add(9);
+    DisplayList.getItems().add(10);
+    DisplayList.getSelectionModel().selectFirst();
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,80 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+  /**
+   * Setting the stage for the scene
+   *
+   * @param primaryStage
+   * @throws Exception
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Production Line Tracker");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+  }
+
+  /**
+   * Database connectivity
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:.res/ProductDatabase";
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    try {
+
+      // STEP 1: Register JDBC driver
+
+      Class.forName(JDBC_DRIVER);
+
+      // STEP 2: Open a connection
+
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      // STEP 3: Execute a query
+
+      stmt = conn.createStatement();
+
+      String sql = "SELECT * FROM JOBS";
+
+      ResultSet rs = stmt.executeQuery(sql);
+
+      while (rs.next()) {
+
+        System.out.println(rs.getString(1));
+      }
+
+      // STEP 4: Clean-up environment
+
+      stmt.close();
+
+      conn.close();
+
+    } catch (ClassNotFoundException e) {
+
+      e.printStackTrace();
+
+    } catch (SQLException e) {
+
+      e.printStackTrace();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+    launch(args);
+  }
 }
